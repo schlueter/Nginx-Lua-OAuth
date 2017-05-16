5ce2dbbff937f115c8f285523fdd3a872c4d564e
Schlueter <bs@schlueter.blue>
2017-05-16 19:06:53 -0400

    More work

--- access-oauth-github.lua
+++ access-oauth-github.lua
@@ -13,6 +13,7 @@ local client_secret = ngx.var.oauth_client_secret
 local authorization_url = ngx.var.oauth_authorization_url
 
 local authorization_callback_uri = ngx.var.oauth_authorization_callback_uri or '/oauth/signin'
+local provider_uri = ngx.var.oauth_provider_uri or '/oauth/provider'
 local proxy_api_uri = ngx.var.oauth_proxy_api_uri or '/oauth/api/'
 local signout_uri = ngx.var.oauth_signout_uri or '/oauth/signout'
 local token_secret = ngx.var.oauth_token_secret or 'notsosecret'
@@ -26,7 +27,7 @@ local client_secret = ngx.var.oauth_client_secret
 local valid_org = ngx.var.oauth_org
 local blacklist = string.gmatch(ngx.var.oauth_blacklist, "(%g+) ?")
 
-local cookie_tail = ";Domain="..domain
+local cookie_tail = "; Domain=" .. domain .. '; HttpOnly; Path=/'
 
 
 local function handle_subrequest_error(response)
@@ -46,7 +47,7 @@ end
 local function request_access_token(code)
   ngx.log(ngx.ERR, 'Requesting access token with code ' .. code)
   local res = ngx.location.capture(
-    authorization_callback_uri,
+    authorization_callback_provider,
     { method=ngx.HTTP_POST
     , args={ client_id=client_id
              , client_secret=client_secret
@@ -54,12 +55,13 @@ local function request_access_token(code)
              , redirect_uri=redirect_uri
              -- , state=state
              }})
-  err = handle_subrequest_error(authorization_callback_uri, res)
+  err = handle_subrequest_error(res)
   if err then
     ngx.log(ngx.ERR, "Got error during access token request: " .. err)
     ngx.say("got error during access token request: " .. err)
     return ngx.exit(ngx.HTTP_FORBIDDEN)
   else
+    ngx.log(ngx.ERR, "Decoded access token request: " .. res.body)
     return ngx.decode_args(res.body)
   end
 end
@@ -69,7 +71,7 @@ local function provider_api_request(api_uri, token)
   local api_request_uri = proxy_api_uri .. api_uri
   ngx.log(ngx.DEBUG, 'Making subrequest to ' .. api_request_uri .. " with token " .. token)
   local api_res = ngx.location.capture(api_request_uri)
-  err = handle_subrequest_error(api_request_uri, api_res)
+  err = handle_subrequest_error(api_res)
   if err then
     ngx.log(ngx.ERR, "Got error during access token request: " .. err)
     ngx.say("got error during access token request: " .. err)
@@ -85,7 +87,7 @@ end
 local function redirect_to_auth()
   local auth_url = authorization_url .. "?" .. ngx.encode_args(
     { client_id=client_id
-    , redirect_uri=redirect_uri .. '?' .. ngx.encode_args({ redirect_uri=uri })
+    -- , redirect_uri=redirect_uri .. '?' .. ngx.encode_args({ redirect_uri=uri })
     , scope = scope
     })
   ngx.log(ngx.ERR, 'redirecting to ' .. auth_url)
@@ -121,13 +123,6 @@ local function validate(login, access_token, token)
     login = profile["login"]
   end
 
-  for bad_login in blacklist do
-    if login == bad_login then
-      ngx.log(ngx.ERR, "User " .. login .. " is blacklisted")
-      return ngx.exit(ngx.HTTP_FORBIDDEN)
-    end
-  end
-  ngx.log(ngx.ERR, "User " .. login .. " is not blacklisted")
 
   if access_token then
     if validate_orgs(access_token) then
@@ -152,6 +147,7 @@ end
 
 local function authorize()
 
+  ngx.log(ngx.ERR, "Checking uri " .. uri .. " agaist callback uri " .. authorization_callback_uri)
   if uri ~= authorization_callback_uri then
     return redirect_to_auth()
   end
@@ -162,16 +158,28 @@ local function authorize()
     return ngx.exit(ngx.HTTP_FORBIDDEN)
   end
 
-  local token = request_access_token(uri_args["code"])
+  if not uri_args["code"] then
+    ngx.log(ngx.ERR, "Invalid request: no code for authorization")
+    return ngx.exit(ngx.HTTP_FORBIDDEN)
+  end
+
+  local access_token_res = request_access_token(uri_args["code"])
+
   local login, token = validate(nil, access_token_res.access_token, nil)
 
   if token then
-    local expiry = ";Max-Age=" .. (60*60*24*14)
-    ngx.header["Set-Cookie"] = {
+    local expiry = "; Max-Age=" .. (ngx.time() + 60*60*24*14)
+    local cookies = {
       "OAuthLogin=" .. ngx.escape_uri(login) .. cookie_tail .. expiry,
       "OAuthAccessToken=" .. ngx.escape_uri(token) .. cookie_tail .. expiry,
     }
-    local redirect = uri_args["redirect_uri"] or '/'
+
+    for index, cookie in pairs(cookies) do
+      ngx.log(ngx.ERR, "Setting cookie " .. cookie " " .. index)
+    end
+
+    ngx.header["Set-Cookie"] = cookies
+    local redirect = '/'
     ngx.log(ngx.ERR, "Redirecting to " .. redirect)
     return ngx.redirect(redirect)
   else
@@ -184,7 +192,29 @@ end
 local function is_authorized()
   local login = ngx.unescape_uri(ngx.var.cookie_OAuthLogin)
   local token = ngx.unescape_uri(ngx.var.cookie_OAuthAccessToken)
-  return validate(login, nil, token)
+  ngx.log(ngx.ERR, "Checking authorization of " .. login .. " with token " .. token)
+
+  if login == '' or token == '' then
+    ngx.log(ngx.ERR, "Missing login or token")
+    return false
+  end
+
+  for bad_login in blacklist do
+    if login == bad_login then
+      ngx.log(ngx.ERR, "User " .. login .. " is blacklisted")
+      return ngx.exit(ngx.HTTP_FORBIDDEN)
+    end
+  end
+  ngx.log(ngx.ERR, "User " .. login .. " is not blacklisted")
+
+  local expected_token = ngx.encode_base64(ngx.hmac_sha1(token_secret, domain .. login))
+  ngx.log(ngx.ERR, "Checking " .. login .. "'s token, " .. (token or 'nil') .. ", against expected token, " .. expected_token)
+  if token ~= expected_token then
+    ngx.log(ngx.ERR, "User " .. login .. " has a bad token")
+    return false
+  end
+  ngx.log(ngx.ERR, login .. " is authorized")
+  return true
 end
 
 local function handle_signout()
@@ -200,4 +230,8 @@ handle_signout()
 
 if not is_authorized() then
   authorize()
+end
+
+if uri == authorization_callback_uri then
+  uri = '/'
 end
