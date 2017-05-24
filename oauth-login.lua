de157197206179d029afec0fddee43ebc6cadb9d
Schlueter <bs@schlueter.blue>
2017-05-24 16:53:55 -0400

    Call get_uri_args correctly

--- oauth-login.lua
+++ oauth-login.lua
@@ -2,7 +2,7 @@ local authorize_url = oauth_authorize_url or ngx.var.oauth_authorize_url
 local client_id = oauth_client_id or ngx.var.oauth_client_id
 local scope = oauth_scope or ngx.var.oauth_scope or 'read:org'
 local callback_url = oauth_callback_url or ngx.var.oauth_callback_url
-local target_uri = ngx.req.get_uri_args['target_uri'] or '/'
+local target_uri = ngx.req.get_uri_args()['target_uri'] or '/'
 
 
 local auth_url = authorize_url .. "?" .. ngx.encode_args(
